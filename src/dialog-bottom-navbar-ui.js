52f92783557d2333adecc1e9da2720b68f4009e9
David D'Amico <david.damico@cbc.ca>
2015-06-24 16:20:51 -0400

    adds existing component code

---
+++ src/dialog-bottom-navbar-ui.js
@@ -0,0 +1,25 @@
+define(['text!./dialog-bottom-navbar.html'],
+    function(template) {
+        'use strict';
+
+        var DialogBottomNavbar = function(params, componentInfo) {
+
+            var self = this;
+
+            self.closeLabel = params.closeLabel;
+            self.saveLabel = params.saveLabel;
+
+            self.save = params.save;
+            self.close = params.close;
+
+        };
+
+        return {
+            viewModel: {
+                createViewModel: function(params, componentInfo) {
+                    return new DialogBottomNavbar(params, componentInfo);
+                }
+            },
+            template: template
+        };
+    });
