ee5bd4f245b2d17c51ae853c0547a6e4e3dd6f90
David D'Amico <david.damico@cbc.ca>
2015-07-06 16:08:27 -0400

    adds ability for client to pass in a desired size for action buttons

--- src/dialog-bottom-navbar-ui.js
+++ src/dialog-bottom-navbar-ui.js
@@ -9,6 +9,14 @@ define(['text!./dialog-bottom-navbar.html'],
             self.closeLabel = params.closeLabel;
             self.saveLabel = params.saveLabel;
             self.saveIcon = params.saveIcon;
+            
+            // map size params to bootstrap classes
+            self.sizeMap = {
+                xs: 'btn-xs',
+                sm: 'btn-sm',
+                lg: 'btn-lg'
+            }
+            self.buttonSizeClass = !_.isUndefined(params.buttonSize) && !_.isUndefined(self.sizeMap[params.buttonSize]) ? self.sizeMap[params.buttonSize] : '';
 
             self.save = params.save;
             self.close = params.close;
